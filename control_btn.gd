86abe9e3563a0974b2107f377678bccf58d8b206
Victor Pukman <pukman.victor@gmail.com>
2022-06-13 21:15:02 +0300

    add pause

--- control_btn.gd
+++ control_btn.gd
@@ -4,15 +4,20 @@ signal reload
 export(int) var start_bar_index = 0
 
 func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
 	$BarIndex.value = start_bar_index
 
 func _on_ReloadButton_pressed():
 	print(start_bar_index)
 	emit_signal("reload", start_bar_index)
 	$CurrBarIndex.text = ""
+	get_tree().paused = false # back to game
 
 func _on_BarIndex_value_changed(value):
 	start_bar_index = int(value)
 
 func on_bar_index_updated(index):
 	$CurrBarIndex.text = str(index)
+
+func _on_PauseButton_pressed():
+	get_tree().paused = !get_tree().paused
