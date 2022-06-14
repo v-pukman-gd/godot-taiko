6fcf0f854fea1926c4f98d63192d87c1fccc9a53
Victor Pukman <pukman.victor@gmail.com>
2022-06-15 00:13:07 +0300

    fix start px

--- game.gd
+++ game.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 var audio
 var map
-var audio_path = "res://songs/Middle_of_The_Night/Middle_of_The_Night_CC_BY_SA.ogg"
-var map_path = "res://songs/Middle_of_The_Night/Middle_of_The_Night_CC_BY_SA.mboy"
+var audio_path = "res://songs/hansatom_-_Vitamins_CC_BY_NC/hansatom_-_Vitamins_CC_BY_NC.ogg" #= "res://songs/Middle_of_The_Night/Middle_of_The_Night_CC_BY_SA.ogg"
+var map_path = "res://songs/hansatom_-_Vitamins_CC_BY_NC/hansatom_-_Vitamins_CC_BY_NC.mboy" #= "res://songs/Middle_of_The_Night/Middle_of_The_Night_CC_BY_SA.mboy"
 
 var tempo
 var bar_length_in_m
@@ -11,7 +11,7 @@ var quarter_time_in_sec
 var speed
 var note_scale
 var start_pos_in_sec
-#var start_pos_in_px
+var start_pos_in_px
 var pre_start_length
 
 var data_ready = false
@@ -44,7 +44,7 @@ func setup():
 	speed = bar_length_in_m/float(4*quarter_time_in_sec) # each bar has 4 quarters # 
 	note_scale = bar_length_in_m/float(4*400)
 	start_pos_in_sec = (float(map.start_pos)/400.0) * quarter_time_in_sec
-	#start_pos_in_px = start_pos_in_sec * speed
+	start_pos_in_px = start_pos_in_sec * speed
 	pre_start_length = 0 #bar_length_in_m # bars count #bar_length_in_m
 	
 	music = music_scn.instance()
@@ -57,6 +57,7 @@ func setup():
 	
 	track = track_scn.instance()
 	#track.pre_start_length = pre_start_length
+	track.curr_bar_x = start_pos_in_px
 	track.curr_bar_index = start_bar_index
 	track.bars_data = map.tracks[0].bars
 	track.speed = speed
